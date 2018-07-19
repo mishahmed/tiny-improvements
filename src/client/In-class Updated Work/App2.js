be8f3cd54a68d1c30d28023aa87d42dee5e7003c
Mishaal Ahmed <mahmed@salesforce.com>
2018-07-18 20:10:21 -0400

    Changed Class 4

--- src/client/In-class Updated Work/App2.js
+++ src/client/In-class Updated Work/App2.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Col, Container, Row, Button, Card, CardBody, Form, FormGroup, Input, Label } from "reactstrap";
-import AwardCard from "./component/AwardCard";
 
 class App extends Component {
 
@@ -9,49 +8,28 @@ class App extends Component {
     this.state = {
       users: [
         {
-          name: "Alia",
-          userId: 10457,
-          position: "Solutions Engineer"
-        },
-        {
-          name: "Cody",
-          userId: 10850,
-          position: "Senior Functional Consultant"
-        },
-        {
+           name: "Alia",
+           userId: 10457,
+           position: "Solutions Engineer"
+         },
+         {
+           name: "Cody",
+           userId: 10850,
+           position: "Senior Functional Consultant"
+         },
+         {
           name: "Ana",
           userId: 32481,
           position: "Lead Solutions Engineer"
         },
-        {
-          name: "Leon",
-          userId: "02481",
-          position: "Lead Solutions Engineer"
-        }
-      ],
-
-      awards: [
-        {
-          id: 1,
-          title: "Best Boss Award!",
-          comment: "Thanks for always looking out for us."
-        },
-        {
-          id: 2,
-          title: "Longest Commute Award!",
-          comment: "I can't believe Leslie makes it to work as often as she does."
-        },
-        {
-          id: 3,
-          title: "Most likely to nap at work!",
-          comment: "Maybe you need more coffee."
-        }
-      ]
+         {
+           name: "Leon",
+           userId: "02481",
+           position: "Lead Solutions Engineer"
+         }
+       ]
     }
   }
-
-
-
 
   render() {
     return (
@@ -71,9 +49,15 @@ class App extends Component {
             </Card>
           </Col>
           <Col md="12" lg="9">
-
-            {this.state.awards.map(e => <p>{<AwardCard title={e.title} comment={e.comment}/>}</p>)}
-
+            <Card>
+              <CardBody className="mx-auto">
+                <img alt="award" src="http://www.pngmart.com/files/3/Award-PNG-Photos.png" width="50px" />
+                <p>Badge Name</p>
+                <img alt="avatar" src="https://www.iranketab.ir/Images/user.jpg" width="100px" />
+                <h2> Heading </h2>
+                <p>Conversion stealth influencer business-to-business entrepreneur hypotheses investor customer deployment metrics learning curve direct mailing long tail mass market. Pitch iteration stock android business-to-consumer bandwidth seed round user experience paradigm shift channels equity pivot. Metrics partner network validation responsive web design first mover advantage backing research & development market mass market innovator sales infrastructure.</p>
+              </CardBody>
+            </Card>
           </Col>
         </Row>
         <Row>
@@ -83,17 +67,17 @@ class App extends Component {
               <FormGroup>
                 <Label>Give Kudos to</Label>
                 <Input type="select">
-
+              
                   {this.state.users.map(user => <option>({user.userId}) {user.name} {user.position}</option>)}
                   <option>{this.state.users.length}</option>
-
+              
                 </Input>
               </FormGroup>
               <FormGroup>
-                <Input type="text" placeholder="Kudos Title" />
+                <Input type="text"  placeholder="Kudos Title" />
               </FormGroup>
               <FormGroup>
-                <Input type="textarea" placeholder="Kudos text" />
+                <Input type="textarea"  placeholder="Kudos text" />
               </FormGroup>
             </Form>
           </Col>
